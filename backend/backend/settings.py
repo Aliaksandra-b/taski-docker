d4e1cb6d908919be53b4c980eda1aa5de7b54381
Aliaksandra Bichan <aliaksandra.bichan@yandex.by>
2025-01-20 22:22:09 +0300

    ошибка api_2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['taskipracticum.zapto.org', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['taskipracticum.zapto.org', '127.0.0.1', 'localhost', '51.250.29.150']
 
 
 # Application definition
